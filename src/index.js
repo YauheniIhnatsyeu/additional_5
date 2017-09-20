f052ea474859114cdeea82e3f8edbb95f691d79c
YauheniIhnatsyeu <tyz1@list.ru>
2017-09-20 21:21:00 +0300

    Azz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+var openedBrackArray = [];
+var bracketsConf = [];
+
+
+module.exports =
+    function check(str, bracketsConfig) {
+        bracketsConf = bracketsConfig;
+        openedBrackArray = [];
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] == findBraketPair(str[i])) {
+                if (str[i] == findBraketPair(str[i], true))
+                    if (deleteOpenBraket(str[i])) {
+                        continue;
+                    }
+                openedBrackArray.push(str[i]);
+            } else if (str[i] == findBraketPair(str[i], true)) {
+                if (!deleteOpenBraket(str[i])) {
+                    return false;
+                }
+            }
+        }
+        openedBrackArray;
+        if (openedBrackArray.length != 0) return false;
+        return true;
+    }
+
+
+function deleteOpenBraket(type) {
+    let del = false;
+    if (openedBrackArray[openedBrackArray.length - 1] == findBraketPair(type)) {
+        openedBrackArray.splice(openedBrackArray.length - 1, 1);
+        return true;
+    }
+    return false;
+}
+
+
+
+function findBraketPair(bracket, close = false) {
+    var position = 0;
+    //[['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]
+    bracketsConf.forEach((elem, index) => {
+        if (elem.indexOf(bracket) >= 0) position = index;
+    })
+    return close ? bracketsConf[position][1] : bracketsConf[position][0];
 }
